8c94565c602e854964dbd70d96abf3fab8e10e1d
HoangAnhNguyen269 <74226464+HoangAnhNguyen269@users.noreply.github.com>
2022-01-13 23:17:29 +0800

    fix when the screen is rotated, the app is crashed. add border for the table in high score screen

--- app/src/main/java/au/edu/jcu/learn/cp3406/practical/educationalgamev1/HighScoreActivity.java
+++ app/src/main/java/au/edu/jcu/learn/cp3406/practical/educationalgamev1/HighScoreActivity.java
@@ -22,60 +22,67 @@ public class HighScoreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_score);
 
-        SQLiteOpenHelper gameDatabaseHelper = new GameDatabaseHelper(this);
+
         try {
+            SQLiteOpenHelper gameDatabaseHelper = new GameDatabaseHelper(this);
              db = gameDatabaseHelper.getReadableDatabase();
              cursor = db.query(GameDatabaseHelper.RESULT_TABLE, new String[]{"_id", GameDatabaseHelper.USER_NAME_COLUMN,GameDatabaseHelper.SUBJECT_COLUMN,GameDatabaseHelper.SCORE_COLUMN, GameDatabaseHelper.AVERAGE_SECONDS_COLUMN},
                     null, null, null, null, GameDatabaseHelper.SCORE_COLUMN +" DESC" );
              displayTopResult();
-        }catch(SQLiteException e) {
+        }catch(Exception e) {
             Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
             toast.show();
-            Log.i("load successfully?", "not ok");
+            Log.i("can load", "not ok");
         }
 
     }
     public void displayTopResult(){
-        cursor.moveToFirst();
-        for (int i = 0; i <cursor.getCount() ; i++) {
+            cursor.moveToFirst();
+            for (int i = 0; i <cursor.getCount() ; i++) {
 
-            String userName;
-            userName = cursor.getString(1);
+                String userName;
+                userName = cursor.getString(1);
 
-            String subject;
-            subject = cursor.getString(2);
+                String subject;
+                subject = cursor.getString(2);
 
-            float score;
-            score = cursor.getFloat(3);
+                float score;
+                score = cursor.getFloat(3);
 
-            float avgSeconds;
-            avgSeconds = cursor.getFloat(4);
+                float avgSeconds;
+                avgSeconds = cursor.getFloat(4);
 
-            //check the availability of the result table
-            Log.i("Score", String.valueOf(score));
-            Log.i("User", userName);
-            Log.i("Subject", subject);
-            Log.i("avg second", String.valueOf(avgSeconds));
+                //check the availability of the result table
+                Log.i("Score", String.valueOf(score));
+                Log.i("User", userName);
+                Log.i("Subject", subject);
+                Log.i("avg second", String.valueOf(avgSeconds));
 
-            //inflate the fragment
-            ResultRowFragment rowFragment = new ResultRowFragment(userName, subject, score,avgSeconds);
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            ft.add(R.id.result_table,rowFragment);
-            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-            ft.commit();
-            //add
-            cursor.moveToNext();
-        }
+                //inflate the fragment
+                ResultRowFragment rowFragment = new ResultRowFragment(userName, subject, score,avgSeconds);
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.add(R.id.result_table,rowFragment);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.commit();
+                //add
+                cursor.moveToNext();
+            }
+
+
     }
-
-
 
     @Override
     public void onDestroy(){
+        if(db!= null && cursor!= null){
+            db.close();
+            cursor.close();
+        }
         super.onDestroy();
-        db.close();
-        cursor.close();
+
     }
+
+
+
 
     @Override
     public void onRestart() {
